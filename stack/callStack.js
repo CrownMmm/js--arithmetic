9f0c2c1e2026fdcfff4e57eb466f16d334d63311
zhangjiaming <823600537@qq.com>
2021-01-16 03:11:16 +0800

    feat:链表 集合

--- stack/callStack.js
+++ stack/callStack.js
@@ -1,1 +1,31 @@
-const fun1 = () => { };
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    const stack = [];
+    for (let i = 0; i < s.length; i++) {
+        const c = s[i];
+        if (c === '(' | c === '{' | c === '[') {
+            stack.push(c);
+        } else {
+            t = stack[stack.length - 1];
+            if (
+                (t === '(' && c === ')') ||
+                (t === '{' && c === '}') ||
+                (t === '[' && c === ']')
+            ) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
+
+
+let test1 = "()";
+let test2 = "()[]{}";
+// console.log(isValid(test1), isValid(test2));
+isValid(test2);
